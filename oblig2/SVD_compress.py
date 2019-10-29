644843b1eb99fd867321a41ddc662daea8fd88b6
joaakerh <joaakerh@student.matnat.uio.no>
2019-10-29 14:29:57 +0100

    Almost done

--- oblig2/SVD_compress.py
+++ oblig2/SVD_compress.py
@@ -16,6 +16,7 @@ def plot_compression_ratio():
 
     plt.plot([25,k_max], [1,1], "r--", alpha = 0.5, label = "1")
     plt.legend()
+    plt.savefig("images/CRs.pdf")
     plt.show()
 
 def SVD_size(U, S, V):
@@ -67,13 +68,16 @@ im3 = rgb2gray(im3)
 
 
 image_list = [im, im2, im3]
-k_list = [int(1280*2**(-i)) for i in range(6)]
+image_names = ["chessboard", "jellyfish", "new-york"]
+k_list = [int(1280*2**(-i)) for i in range(11)]
 print(k_list)
 
-for image in image_list:
+for image, image_name in zip(image_list, image_names):
     plt.figure()
     plt.imshow(image, cmap="gray")
     plt.title("Original")
+    fn = "images/{:s}-original.pdf".format(image_name)
+    plt.savefig(fn)
 
     for k in k_list:
         U, S, V, k = SVD_compress(image, k)
@@ -82,9 +86,19 @@ for image in image_list:
         plt.figure()
         plt.imshow(im_compressed, cmap="gray")
         plt.title("Compressed, k = {:d}, CR = {:f}".format(k, CR))
+        fn = "images/{:s}-{:04d}.pdf".format(image_name, k)
+        plt.savefig(fn)
 
     U, S, V, k = SVD_compress(image, np.min(image.shape))
     plt.figure()
+    cross_1 = np.argmin(np.abs(S - 1))
+    print("First 5 singular values:", S[:5])
+    print("Singular values cross 1 at k={:d}".format(cross_1))
+    print("Singular values at cross:", S[cross_1-3:cross_1+2])
     plt.semilogy(S)
+    plt.title("Singular values")
+    plt.xlabel("index")
+    fn = "images/{:s}-svs.pdf".format(image_name, k)
+    plt.savefig(fn)
 
     plt.show()
